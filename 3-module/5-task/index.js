96a2cf0f84cbaaeaa7619c4b805693f0d846de61
Ivan Talalaev <ivan@glossa.systems>
2021-04-11 23:18:08 +0300

    Найти min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
-  // ваш код...
+  let splitArray = str.replace(/\s/g,',').split(',');
+  let numArr = splitArray.filter(item => item > 0 || item < 0 );
+  let result = {};
+  result.min = Math.min(...numArr);
+  result.max = Math.max(...numArr);
+  return result;
 }
