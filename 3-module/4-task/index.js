88816733c73baa8b0342de3501cbc087dec2f85d
Ivan Talalaev <ivan@glossa.systems>
2021-04-13 09:47:14 +0300

    Исправил функцию showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,13 +1,6 @@
 function showSalary(users, age) {
-  let filtering = users.filter((arr) => arr.age <= age);
-  let clearArr = function (data) {
-    let newArr = [];
-    for (let i = 0; i < data.length; i++) {
-      newArr.push(data[i].name + ", " + data[i].balance + "\n");
-    }
-    return newArr;
-  };
-  let clearLastN = clearArr(filtering).join(' ');
-  let n = clearLastN.length;
-  return clearLastN.slice(0, n - 1);
+  return users
+    .filter((item) => item.age <= age)
+    .map((item) => `${item.name}, ${item.balance}`)
+    .join("\n");
 }
