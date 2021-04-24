790c9b1f57a38dc3b156961618ebe46095fab90b
Ivan Talalaev <ivan@glossa.systems>
2021-04-24 23:43:58 +0300

    Модуль 5 таск 3: Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,29 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowLeft = document.querySelector(".carousel__arrow_left");
+  let carouselArrowRight = document.querySelector(".carousel__arrow_right");
+  let slider = document.querySelector(".carousel__inner");
+  let carouselInner = document.querySelector('.carousel__inner');
+  let sliderWidth = carouselInner.offsetWidth;
+  let x = 0;
+  carouselArrowLeft.style.display = 'none';
+  carouselArrowLeft.addEventListener('click', function() {
+    if (x < 0){
+      x += sliderWidth;
+      slider.style.transform = `translateX(${x}px)`;
+      carouselArrowRight.style.display = '';
+      if (x == 0) {
+        this.style.display = 'none';
+      }
+    }
+  } );
+  carouselArrowRight.addEventListener('click', function() {
+    if (x > -2964) {
+      x -= sliderWidth;
+      slider.style.transform = `translateX(${x}px)`;
+      carouselArrowLeft.style.display = '';
+      if (x < -1976) {
+        this.style.display = 'none';
+      }
+    }
+  } );
 }
