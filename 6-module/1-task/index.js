8b813e1523e4871e23d5630259f1817378443f41
Ivan Talalaev <ivan@glossa.systems>
2021-04-25 15:50:35 +0300

    Модуль 6 таск 1: Таблица с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.data = rows;
+    this.template = '';
+    this.render()
+  }
+
+  render() {
+    this.elem = document.createElement('TABLE');
+    this.elem.className = 'new-table'; //под вопросом
+    this.elem.insertAdjacentHTML("beforeend", ` <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+      </thead>`);
+    this.elem.addEventListener('click', event => {
+      const isButton = event.target.nodeName === 'BUTTON';
+      if (isButton) {
+        event.target.closest('tr').remove();
+      }
+    });
+      for (const { name, age, salary, city } of this.data) {
+				this.template += `
+          <tr>
+            <td>${name}</td>
+            <td>${age}</td>
+            <td>${salary}</td>
+            <td>${city}</td>
+            <td><button>X</button></td>
+          </tr>
+				`
+			}
+      this.elem.insertAdjacentHTML('beforeend', this.template);
+      return this.elem;
   }
 }
